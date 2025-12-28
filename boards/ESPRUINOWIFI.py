f45403e51cddc18ad02367ac14c3f0092211414e
Keron Sepp <32654913+Keron320@users.noreply.github.com>
2025-12-28 17:34:33 +0200

    Update ESPRUINOWIFI.py
    3

--- boards/ESPRUINOWIFI.py
+++ boards/ESPRUINOWIFI.py
@@ -44,6 +44,8 @@ info = {
    ],
    'makefile' : [
      'DEFINES+=-DESPR_OFFICIAL_BOARD', # Don't display the donations nag screen
+  "DEFINES+=-DNO_VIRTUAL_PIN",
+  "DEFINES+=-DLCD_SPI_UNBUF_LEN=64",
      'DEFINES+=-DUSE_USB_OTG_FS=1 -DESPRUINOWIFI',
      'DEFINES+=-DPIN_NAMES_DIRECT=1', # Package skips out some pins, so we can't assume each port starts from 0
      'WIZNET=1', # Add support for W5500 by default (not CC3000)
